5022fb82072c3dfcd4ce57f1bceadb2df99923dc
José Wanderson <wanderson.ballack@hotmail.com>
2022-03-06 11:20:04 -0300

    Inicialização da seção Nossos Produtos

---
+++ main.js
@@ -0,0 +1,9 @@
+const imagemNossosProdutos = document.querySelectorAll('.img__produtos');
+
+function adicionarImagens() {
+    for(let i = 0; i < imagemNossosProdutos.length; i++) {
+        
+    }
+}
+
+adicionarImagens();
